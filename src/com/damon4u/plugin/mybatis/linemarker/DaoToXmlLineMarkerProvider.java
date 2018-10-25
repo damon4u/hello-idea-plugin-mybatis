5ac0a41b6197e3b4fefc0e0a769f50ea6fc985b1
damon4u <bjweidongjun@corp.netease.com>
2018-10-25 22:21:01 +0800

    daoToXml and xmlToDao lineMarkerProvider

--- src/com/damon4u/plugin/mybatis/linemarker/DaoToXmlLineMarkerProvider.java
+++ src/com/damon4u/plugin/mybatis/linemarker/DaoToXmlLineMarkerProvider.java
@@ -4,6 +4,7 @@ import com.damon4u.plugin.mybatis.dom.model.IdDomElement;
 import com.damon4u.plugin.mybatis.service.JavaService;
 import com.damon4u.plugin.mybatis.util.Icons;
 import com.damon4u.plugin.mybatis.util.JavaUtils;
+import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
@@ -11,6 +12,7 @@ import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.CommonProcessors;
 import com.intellij.util.xml.DomElement;
 import org.jetbrains.annotations.NotNull;
@@ -25,6 +27,8 @@ import java.util.Collection;
  */
 public class DaoToXmlLineMarkerProvider extends RelatedItemLineMarkerProvider {
 
+    private static final Function<DomElement, XmlTag> FUNCTION = DomElement::getXmlTag;
+
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
         if (element instanceof PsiNameIdentifierOwner && JavaUtils.isElementWithinInterface(element)) {
@@ -34,7 +38,7 @@ public class DaoToXmlLineMarkerProvider extends RelatedItemLineMarkerProvider {
             if (!results.isEmpty()) {
                 NavigationGutterIconBuilder<PsiElement> builder = NavigationGutterIconBuilder.create(Icons.DAO_TO_XML_LINE_MARKER_ICON)
                         .setAlignment(GutterIconRenderer.Alignment.CENTER)
-                        .setTargets(Collections2.transform(results, DomElement::getXmlTag))
+                        .setTargets(Collections2.transform(results, FUNCTION))
                         .setTooltipTitle("Navigation to target in mapper xml");
                 PsiElement nameIdentifier = ((PsiNameIdentifierOwner) element).getNameIdentifier();
                 if (nameIdentifier != null) {

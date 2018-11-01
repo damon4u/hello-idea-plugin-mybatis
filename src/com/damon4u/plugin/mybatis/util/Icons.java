0feec8495fd5ca62ed433a032acdcdb9b336aa85
damon4u <bjweidongjun@corp.netease.com>
2018-11-01 11:40:09 +0800

    icon and description

--- src/com/damon4u/plugin/mybatis/util/Icons.java
+++ src/com/damon4u/plugin/mybatis/util/Icons.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 
 public interface Icons {
 
-    Icon DAO_TO_XML_LINE_MARKER_ICON = IconLoader.getIcon("/images/daoToXml.png");
-
-    Icon XML_TO_DAO_LINE_MARKER_ICON = IconLoader.getIcon("/images/xmlToDao.png");
+    Icon DAO_TO_XML_LINE_MARKER_ICON = IconLoader.getIcon("/images/right1.png");
+    
+    Icon XML_TO_DAO_LINE_MARKER_ICON = IconLoader.getIcon("/images/left1.png");
 
 }
